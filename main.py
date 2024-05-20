559bee90879f2d122a0436284fca69c4a72659ce
Kate <katevoronina128@gmail.com>
2024-05-21 01:48:53 +0400

    Initial commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import Telegram.tg
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
-
+from aiogram import F
 from telegram_menu import BaseMessage, TelegramMenuSession, NavigationHandler
 
 
@@ -33,9 +33,11 @@ GPIO.output(RELE, GPIO.LOW)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-mode1 = "mode1"
-mode2 = "mode2"
-mode3 = "mode3"
+mode1 = "Включить прерывание"
+mode2 = "Отключить прерывание временно)"
+mode3 = "Отключить прерывание отключено)"
+mode4 = "Посмотреть историю"
+
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
@@ -43,47 +45,48 @@ async def send_welcome(message: types.Message):
        [
            types.KeyboardButton(text=mode1),
            types.KeyboardButton(text=mode2),
-           types.KeyboardButton(text=mode3)
+           types.KeyboardButton(text=mode3),
+           types.KeyboardButton(text=mode4)
        ],
    ]
-   keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
- 
-   await message.reply("Привет", reply_markup=keyboard)
+   keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите режим"
+    )
+   await message.reply(reply_markup=keyboard)
 
-# Обработчик нажатия на кнопку
-@dp.message_handler(commands=[mode1])
-async def button_click(message: types.Message):
+
+
+@dp.message(F.text.lower() == mode1)
+async def with_puree(message: types.Message):
     set_mode1()
     turnoff_mode2()
     turnoff_mode3()
     turnoff_releoff()
 
-
-# Обработчик нажатия на кнопку
-@dp.message_handler(commands=[mode2])
-async def button_click(message: types.Message):
+@dp.message(F.text.lower() == mode2)
+async def without_puree(message: types.Message):
     set_mode2()
     turnoff_mode1()
     turnoff_mode3()
     turnoff_releoff()
    
-   
-# Обработчик нажатия на кнопку
-@dp.message_handler(commands=[mode3])
-async def button_click(message: types.Message):
+@dp.message(F.text.lower() == mode3)
+async def without_puree(message: types.Message):
     set_mode3()
     turnoff_mode2()
     turnoff_mode1()
     turnoff_releoff()
 
+@dp.message(F.text.lower() == mode4)
+async def without_puree(message: types.Message):
+    await message.reply("Так невкусно!") 
+    
+
 sygnal_val = input(SYGNAL)
 
-
-
 if state == State.MODE1:
-    turnoff_mode2()
-    turnoff_mode3()
-    turnoff_releoff()
     digitalWrite(LED1, led1_state)
     if sygnal_val:
         rele_state = 1
@@ -91,18 +94,12 @@ if state == State.MODE1:
         turnoff_mode1()
         set_releoff()
 elif state == State.MODE2:
-    turnoff_mode1()
-    turnoff_mode3()
-    turnoff_releoff()
     current_time = time.time() 
     if mode2_start_time < current_time - MODE2_INTERVAL:
         turnoff_mode2()
         set_mode1()
 
 elif state == State.MODE3:
-    turnoff_mode1()
-    turnoff_mode2()
-    turnoff_releoff()
     currentMillis = time.time() 
     if currentMillis - previousMillis >= BLINK_INTERVAL:
         led1_state = ~led1_state
